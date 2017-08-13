bf2bceb8102e0903e29ed8a70b8e2ded70e6c64f
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-13 14:04:23 -0400

    Corrected signatures to match PAAS type

--- out/cloud_foundry.go
+++ out/cloud_foundry.go
@@ -38,7 +38,7 @@ func (cf *CloudFoundry) Target(organization string, space string) error {
 	return cf.cf("target", "-o", organization, "-s", space).Run()
 }
 
-func (cf *CloudFoundry) CreateRoute(space string, domain string, host string, path string, port int, randomPort bool) error {
+func (cf *CloudFoundry) CreateRoute(space string, domain string, host string, path string, port int32, randomPort bool) error {
 	args := []string{"create-route", space, domain}
 	if len(host) > 0 {
 		args = append(args, "--host", host)
@@ -50,13 +50,13 @@ func (cf *CloudFoundry) CreateRoute(space string, domain string, host string, pa
 	if randomPort {
 		args = append(args, "--random-port")
 	} else if port > 0 {
-		args = append(args, "--port", strconv.Itoa(port))
+		args = append(args, "--port", strconv.Itoa(int(port)))
 	}
 
 	return cf.cf(args...).Run()
 }
 
-func (cf *CloudFoundry) MapRoute(app string, domain string, host string, path string, port int) error {
+func (cf *CloudFoundry) MapRoute(app string, domain string, host string, path string, port int32) error {
 	args := []string{"map-route", app, domain}
 	if len(host) > 0 {
 		args = append(args, "--host", host)
@@ -66,13 +66,13 @@ func (cf *CloudFoundry) MapRoute(app string, domain string, host string, path st
 		args = append(args, "--path", path)
 	}
 	if port > 0 {
-		args = append(args, "--port", strconv.Itoa(port))
+		args = append(args, "--port", strconv.Itoa(int(port)))
 	}
 
 	return cf.cf(args...).Run()
 }
 
-func (cf *CloudFoundry) UnmapRoute(app string, domain string, host string, path string, port int) error {
+func (cf *CloudFoundry) UnmapRoute(app string, domain string, host string, path string, port int32) error {
 	args := []string{"unmap-route", app, domain}
 	if len(host) > 0 {
 		args = append(args, "--host", host)
@@ -82,49 +82,10 @@ func (cf *CloudFoundry) UnmapRoute(app string, domain string, host string, path 
 		args = append(args, "--path", path)
 	}
 	if port > 0 {
-		args = append(args, "--port", strconv.Itoa(port))
+		args = append(args, "--port", strconv.Itoa(int(port)))
 	}
 
 	return cf.cf(args...).Run()
-}
-
-func (cf *CloudFoundry) PushApp(manifest string, path string, currentAppName string) error {
-	args := []string{}
-
-	if currentAppName == "" {
-		args = append(args, "push", "-f", manifest)
-	} else {
-		args = append(args, "zero-downtime-push", currentAppName, "-f", manifest)
-	}
-
-	if path != "" {
-		stat, err := os.Stat(path)
-		if err != nil {
-			return err
-		}
-		if stat.IsDir() {
-			return chdir(path, cf.cf(args...).Run)
-		}
-
-		// path is a zip file, add it to the args
-		args = append(args, "-p", path)
-	}
-
-	return cf.cf(args...).Run()
-}
-
-func chdir(path string, f func() error) error {
-	oldpath, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	err = os.Chdir(path)
-	if err != nil {
-		return err
-	}
-	defer os.Chdir(oldpath)
-
-	return f()
 }
 
 func (cf *CloudFoundry) cf(args ...string) *exec.Cmd {
