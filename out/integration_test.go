c1e7b4dd5f06e06f46fefca33b7a6d7c87eb5bcc
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-13 14:14:02 -0400

    Commented out test from push

--- out/integration_test.go
+++ out/integration_test.go
@@ -1,14 +1,11 @@
 package out_test
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -53,36 +50,9 @@ var _ = Describe("Out", func() {
 				SkipCertCheck: true,
 			},
 			Params: out.Params{
-				ManifestPath:   "project/manifest.yml",
-				Path:           "another-project",
-				CurrentAppName: "awesome-app",
+				Create: []string{"foo.example.com"},
 			},
 		}
-	})
-
-	JustBeforeEach(func() {
-		assetsPath, err := filepath.Abs("assets")
-		Expect(err).NotTo(HaveOccurred())
-
-		stdin := &bytes.Buffer{}
-
-		err = json.NewEncoder(stdin).Encode(request)
-		Expect(err).NotTo(HaveOccurred())
-
-		cmd = exec.Command(binPath, tmpDir)
-		cmd.Stdin = stdin
-		cmd.Dir = tmpDir
-
-		newEnv := []string{}
-		for _, envVar := range os.Environ() {
-			if strings.HasPrefix(envVar, "PATH=") {
-				newEnv = append(newEnv, fmt.Sprintf("PATH=%s:%s", assetsPath, os.Getenv("PATH")))
-			} else {
-				newEnv = append(newEnv, envVar)
-			}
-		}
-
-		cmd.Env = newEnv
 	})
 
 	AfterEach(func() {
@@ -108,144 +78,17 @@ var _ = Describe("Out", func() {
 			Expect(response.Version.Timestamp).To(BeTemporally("~", time.Now(), time.Second))
 
 			// shim outputs arguments
-			Expect(session.Err).To(gbytes.Say("cf api https://api.run.pivotal.io --skip-ssl-validation"))
-			Expect(session.Err).To(gbytes.Say("cf auth awesome@example.com hunter2"))
-			Expect(session.Err).To(gbytes.Say("cf target -o org -s space"))
-			Expect(session.Err).To(gbytes.Say("cf zero-downtime-push awesome-app -f %s",
-				filepath.Join(tmpDir, "project/manifest.yml"),
-			))
-			Expect(session.Err).To(gbytes.Say(filepath.Join(tmpDir, "another-project")))
+			// Expect(session.Err).To(gbytes.Say("cf api https://api.run.pivotal.io --skip-ssl-validation"))
+			// Expect(session.Err).To(gbytes.Say("cf auth awesome@example.com hunter2"))
+			// Expect(session.Err).To(gbytes.Say("cf target -o org -s space"))
+			// Expect(session.Err).To(gbytes.Say("cf zero-downtime-push awesome-app -f %s",
+			// 	filepath.Join(tmpDir, "project/manifest.yml"),
+			// ))
+			// Expect(session.Err).To(gbytes.Say(filepath.Join(tmpDir, "another-project")))
 
 			// color should be always
 			Expect(session.Err).To(gbytes.Say("CF_COLOR=true"))
 		})
 	})
 
-	Context("when my manifest and file paths contain a glob", func() {
-		var tmpFileManifest *os.File
-		var tmpFileSearch *os.File
-
-		BeforeEach(func() {
-			var err error
-
-			tmpFileManifest, err = ioutil.TempFile(tmpDir, "manifest-some-glob.yml_")
-			Expect(err).NotTo(HaveOccurred())
-			tmpFileSearch, err = ioutil.TempFile(tmpDir, "another-path.jar_")
-			Expect(err).NotTo(HaveOccurred())
-
-			request.Params.ManifestPath = "manifest-*.yml_*"
-			request.Params.Path = "another-path.jar*"
-		})
-
-		Context("when one file matches", func() {
-			It("pushes an application to cloud foundry", func() {
-				session, err := gexec.Start(
-					cmd,
-					GinkgoWriter,
-					GinkgoWriter,
-				)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(session).Should(gexec.Exit(0))
-
-				var response out.Response
-				err = json.Unmarshal(session.Out.Contents(), &response)
-				Expect(err).NotTo(HaveOccurred())
-
-				Expect(response.Version.Timestamp).To(BeTemporally("~", time.Now(), time.Second))
-
-				// shim outputs arguments
-				Expect(session.Err).To(gbytes.Say("cf api https://api.run.pivotal.io --skip-ssl-validation"))
-				Expect(session.Err).To(gbytes.Say("cf auth awesome@example.com hunter2"))
-				Expect(session.Err).To(gbytes.Say("cf target -o org -s space"))
-				Expect(session.Err).To(gbytes.Say("cf zero-downtime-push awesome-app -f %s -p %s",
-					tmpFileManifest.Name(),
-					tmpFileSearch.Name(),
-				))
-
-				// color should be always
-				Expect(session.Err).To(gbytes.Say("CF_COLOR=true"))
-			})
-		})
-
-		Context("when no files match the manifest path", func() {
-			BeforeEach(func() {
-				request.Params.ManifestPath = "nope-*"
-			})
-
-			It("returns an error", func() {
-				session, err := gexec.Start(
-					cmd,
-					GinkgoWriter,
-					GinkgoWriter,
-				)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(session).Should(gexec.Exit(1))
-
-				errMsg := fmt.Sprintf("error invalid manifest path: found 0 files instead of 1 at path: %s", filepath.Join(tmpDir, `nope-\*`))
-				Expect(session.Err).To(gbytes.Say(errMsg))
-			})
-		})
-
-		Context("when more then one file matches the manifest path", func() {
-			BeforeEach(func() {
-				_, err := ioutil.TempFile(tmpDir, "manifest-some-glob.yml_")
-				Expect(err).NotTo(HaveOccurred())
-			})
-
-			It("returns an error", func() {
-				session, err := gexec.Start(
-					cmd,
-					GinkgoWriter,
-					GinkgoWriter,
-				)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(session).Should(gexec.Exit(1))
-				errMsg := fmt.Sprintf("error invalid manifest path: found 2 files instead of 1 at path: %s", filepath.Join(tmpDir, `manifest-\*.yml_\*`))
-				Expect(session.Err).To(gbytes.Say(errMsg))
-			})
-		})
-
-		Context("when no files match the path", func() {
-			BeforeEach(func() {
-				request.Params.Path = "nope-*"
-			})
-
-			It("returns an error", func() {
-				session, err := gexec.Start(
-					cmd,
-					GinkgoWriter,
-					GinkgoWriter,
-				)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(session).Should(gexec.Exit(1))
-
-				errMsg := fmt.Sprintf("error invalid path: found 0 files instead of 1 at path: %s", filepath.Join(tmpDir, `nope-\*`))
-				Expect(session.Err).To(gbytes.Say(errMsg))
-			})
-		})
-
-		Context("when more then one file matches the manifest path", func() {
-			BeforeEach(func() {
-				_, err := ioutil.TempFile(tmpDir, "another-path.jar_")
-				Expect(err).NotTo(HaveOccurred())
-			})
-
-			It("returns an error", func() {
-				session, err := gexec.Start(
-					cmd,
-					GinkgoWriter,
-					GinkgoWriter,
-				)
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(session).Should(gexec.Exit(1))
-				errMsg := fmt.Sprintf("error invalid path: found 2 files instead of 1 at path: %s", filepath.Join(tmpDir, `another-path.jar\*`))
-				Expect(session.Err).To(gbytes.Say(errMsg))
-			})
-		})
-	})
 })
