e143d09e1c0b8b4795a4f613cdcd1518acc83435
Chuck D'Antonio <c_dantonio@acm.org>
2017-08-13 17:10:24 -0400

    Added back setup I too aggressively deleted when getting rid of push stuff

--- out/integration_test.go
+++ out/integration_test.go
@@ -1,8 +1,14 @@
 package out_test
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -17,11 +23,17 @@ import (
 
 var _ = Describe("Out", func() {
 	var (
+		tmpDir  string
 		cmd     *exec.Cmd
 		request out.Request
 	)
 
 	BeforeEach(func() {
+		var err error
+
+		tmpDir, err = ioutil.TempDir("", "cf_resource_out")
+		Expect(err).NotTo(HaveOccurred())
+
 		request = out.Request{
 			Source: resource.Source{
 				API:           "https://api.run.pivotal.io",
@@ -35,6 +47,32 @@ var _ = Describe("Out", func() {
 				Create: []string{"foo.example.com"},
 			},
 		}
+	})
+
+	JustBeforeEach(func() {
+		var err error
+		assetsPath, err := filepath.Abs("assets")
+		Expect(err).NotTo(HaveOccurred())
+
+		stdin := &bytes.Buffer{}
+
+		err = json.NewEncoder(stdin).Encode(request)
+		Expect(err).NotTo(HaveOccurred())
+
+		cmd = exec.Command(binPath, tmpDir)
+		cmd.Stdin = stdin
+		cmd.Dir = tmpDir
+
+		newEnv := []string{}
+		for _, envVar := range os.Environ() {
+			if strings.HasPrefix(envVar, "PATH=") {
+				newEnv = append(newEnv, fmt.Sprintf("PATH=%s:%s", assetsPath, os.Getenv("PATH")))
+			} else {
+				newEnv = append(newEnv, envVar)
+			}
+		}
+
+		cmd.Env = newEnv
 	})
 
 	Context("when a route to create is provided to the resource", func() {
@@ -58,7 +96,7 @@ var _ = Describe("Out", func() {
 			Expect(session.Err).To(gbytes.Say("cf api https://api.run.pivotal.io --skip-ssl-validation"))
 			Expect(session.Err).To(gbytes.Say("cf auth awesome@example.com hunter2"))
 			Expect(session.Err).To(gbytes.Say("cf target -o org -s space"))
-			Expect(session.Err).To(gbytes.Say("cf create-route volcano-base example.com --hostname foo"))
+			Expect(session.Err).To(gbytes.Say("cf create-route space example.com --hostname foo"))
 
 			// color should be always
 			Expect(session.Err).To(gbytes.Say("CF_COLOR=true"))
